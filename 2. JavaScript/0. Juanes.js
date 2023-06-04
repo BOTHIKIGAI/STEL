36be49fe8abad7e6012a8dc613e854221a49a610
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-06-03 20:54:51 -0500

    jaunes update

--- 2. JavaScript/0. Juanes.js
+++ 2. JavaScript/0. Juanes.js
@@ -7,3 +7,26 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open')
 }
+
+
+// formulario de contactenos
+
+var nombreContactenos = document.getElementById("fname");
+var apellidosContactenos = document.getElementById("lname");
+var correoContactenos = document.getElementById("correo");
+var razonContactenos = document.getElementById("razonContacto");
+var errorContactenos = document.getElementById("errorContactenos");
+errorContactenos.style.color = "#23518";
+
+
+function enviarContactenos () {
+    console.log("Enviando formulario...");
+
+    var mensajesError = [];
+
+    if ();
+
+    return false;
+
+
+}

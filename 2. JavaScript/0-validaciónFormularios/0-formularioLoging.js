892f699e4113e9ace11f18bd5d8f95a1bd4ff8f7
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-08-24 09:04:07 -0500

    juanes validaciones

--- 2. JavaScript/0-validaciónFormularios/0-formularioLoging.js
+++ 2. JavaScript/0-validaciónFormularios/0-formularioLoging.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll('#formulario input')
 
 const expresiones = {
 
-    correo: /^(\w*\d+|\w*)(@)(gmail|yahoo|hotmail|outlook)(\.)(com\.co|com)$/, // validación correo
+    usuario: /^(\w*\d+|\w*)(@)(gmail|yahoo|hotmail|outlook)(\.)(com\.co|com)$/, // validación correo
     contraseña: /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\-=\|])(?=.*\d).{8,12}$/ // validación contraseña
 
 }
@@ -14,28 +14,45 @@ const validarFormulario = (e) => {
 
         case "usuario":
 
-            if (expresiones.correo.test()) {
-
-                document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto');
-                document.getElementById('grupo__usuario').classList.add('formulario__grupo-correcto');
-                document.querySelector('#grupo__usuario i').classList.remove('fa-solid', 'fa-circle-xmark');
-                document.querySelector('#grupo__usuario i').classList.add('fa-solid', 'fa-square-check');
-
-            } else {
-
-                document.getElementById('grupo__usuario').classList.remove('formulario__grupo-correcto');
-                document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
-
-            }   
+            validarCampo(expresiones.usuario, e.target, 'usuario');
 
         break;
 
         case "password":
         
+            if (expresiones.contraseña.test()) {
+
+            } else {
+
+            }
+
         break;
 
         
     }
+}
+
+// Función para validar formularios
+
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-xmark');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+
+    } else {
+
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-xmark');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+    }  
 }
 
 inputs.forEach((input) => {

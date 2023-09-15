7b37aec7de945869e0448f6d49466f5299a18720
David-olaya <124094645+David-olaya@users.noreply.github.com>
2023-09-14 20:43:02 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,1 +1,16 @@
+<?php
+    $servidor = '127.0.0.1';
+        $user = 'root';
+        $clave = '';
+        $dbname = 'bd_stel2';
+        $puerto =  '3316';
+        $conn = mysqli_connect($servidor,$user,$clave,$dbname,$puerto);
 
+
+        if(!$conn){
+            die("Error de conexión ".mysqli_connect_error());  
+        }else{
+            echo "Conexión exitosa :D <br>";
+        }
+
+?>

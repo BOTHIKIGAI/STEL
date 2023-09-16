65889a8254fffcb5f5173e195a807d5215ac5b23
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2023-09-16 08:46:16 -0500

    Archivo conexion 	modified:   model/conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-<?php
-    $servidor = '127.0.0.1';
-    $user = 'root';
-    $clave = '';
-    $dbname = 'bd_stel';
-    $puerto = '3316'; //en caso de que el puerto del xampp no sea 3306, agregar la variable
-    $conn = mysqli_connect($servidor,$user,$clave,$dbname,$puerto);
-
-
-        if(!$conn){
-            die("Error de conexión ".mysqli_connect_error());  
-        }else{
-            echo "Conexión exitosa :D <br>";
-        }
-
-?>
-=======
 <?php
     $servidor = '127.0.0.1';
         $user = 'root';
